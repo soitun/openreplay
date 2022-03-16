50a45129e9de0b27bf071f26a642ed6facb94015
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-03-16 12:09:54 +0100

    feat(utilities): EE assist-cluster 1/4

--- ee/utilities/server.js
+++ ee/utilities/server.js
@@ -3,7 +3,7 @@ var {peerRouter, peerConnection, peerDisconnect, peerError} = require('./servers
 var express = require('express');
 const {ExpressPeerServer} = require('peer');
 var socket;
-if (process.env.cluster === "true") {
+if (process.env.redis === "true") {
     console.log("Using Redis");
     socket = require("./servers/websocket-cluster");
 } else {
@@ -51,7 +51,8 @@ peerServer.on('error', peerError);
 app.use('/', peerServer);
 app.enable('trust proxy');
 
-if (process.env.uws !== "true") {
+if (process.env.uws !== "true" && process.env.cluster !== "true") {
+    console.log("NO uWebSocket; NO cluster");
     var wsapp = express();
     wsapp.use(request_logger("[wsapp]"));
     wsapp.use('/assist', socket.wsRouter);
@@ -63,6 +64,65 @@ if (process.env.uws !== "true") {
     wsapp.enable('trust proxy');
     socket.start(wsserver);
     module.exports = {wsserver, server};
+} else if (process.env.uws !== "true" && process.env.cluster === "true") {
+    const numCPUs = require("os").cpus().length;
+    const cluster = require("cluster");
+    const http = require("http");
+    const {setupMaster, setupWorker} = require("@socket.io/sticky");
+    const {createAdapter, setupPrimary} = require("@socket.io/cluster-adapter");
+    console.log(`NO uWebSocket; cluster mode using ${numCPUs} cpu-cores`);
+
+    // var wsapp = express();
+    // wsapp.use(request_logger("[wsapp]"));
+    // wsapp.use('/assist', socket.wsRouter);
+    //
+    // const wsserver = wsapp.listen(PORT + 1, HOST, () => {
+    //     console.log(`WS App listening on http://${HOST}:${PORT + 1}`);
+    //     console.log('Press Ctrl+C to quit.');
+    // });
+    // wsapp.enable('trust proxy');
+    // socket.start(wsserver);
+    // module.exports = {wsserver, server};
+    if (cluster.isMaster) {
+        console.log(`Master (PID:${process.pid}) is running`);
+        const httpServer = http.createServer();
+        // setup sticky sessions
+        setupMaster(httpServer, {
+            loadBalancingMethod: "least-connection",
+        });
+        // setup connections between the workers
+        setupPrimary();
+        // needed for packets containing buffers (you can ignore it if you only send plaintext objects)
+        // Node.js < 16.0.0
+        // cluster.setupMaster({
+        //     serialization: "advanced",
+        // });
+        // Node.js > 16.0.0
+        cluster.setupPrimary({
+            serialization: "advanced",
+        });
+        httpServer.listen(3000);
+
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+
+        cluster.on("exit", (worker) => {
+            console.log(`Worker (PID:${worker.process.pid}) died`);
+            cluster.fork();
+        });
+    } else {
+        console.log(`Worker (PID:${process.pid}) started`);
+        const httpServer = http.createServer();
+        const io = new Server(httpServer);
+        // use the cluster adapter
+        io.adapter(createAdapter());
+        // setup connection with the primary process
+        setupWorker(io);
+        io.on("connection", (socket) => {
+            /* ... */
+        });
+    }
 } else {
     console.log("Using uWebSocket");
     const {App} = require("uWebSockets.js");
